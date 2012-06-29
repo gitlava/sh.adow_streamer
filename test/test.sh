6090c258d4ffc49e1f9713ddd7f5ee5d4195db81
lava <lava@midgard.by>
2012-06-29 23:24:11 +0300

    sh.adow_tracker now included back to the main script in order to avoid wrong usage and simplify packaging fixed stdout/stderr redirection

--- test/test.sh
+++ test/test.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-. ../sh.adow_streamer
+. /usr/lib/sh.adow/sh.adow_streamer
 
-wrp_level 1 -p '[INFO] `date -Is`: ' - ./stdout
+wrp_level 1 -p '[INFO] `date -Is`: '  - ./stdout
 wrp_level 2 -p '[ERROR] `date -Is`: ' - ./stderr
 wrp_level 4 -p '[DEBUG] `date -Is`: ' ./level-4
+wrp_level 6 -- -p '[DEBUG] `date -Is`: ' ./level-4
 
-echo Normal
-echo Error >&2
-echo Level-4 >&4
+echo Normal message
+sleep 1
+echo Error happens >&2
+sleep 1
+echo Level-4 message >&4
+sleep 1
+echo Level-6 message >&6
 
